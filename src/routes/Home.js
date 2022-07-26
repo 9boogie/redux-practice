cd89e02687cfabc6606b2386cb1196f7398a76f3
9boogie <jzero.youngs89@gmail.com>
2022-07-26 08:34:46 -0700

    redux ver

---
+++ src/routes/Home.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import ToDo from "../components/ToDo";
+
+function Home({ toDos, addToDo }) {
+  const [text, setText] = useState("");
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addToDo(text);
+    setText("");
+  };
+
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ui>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDos.id} />
+        ))}
+      </ui>
+    </>
+  );
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { toDos: state };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
